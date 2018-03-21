c7808a053b3b2d3c99d23a198a816e2bb8c2a30d
bakul <bakul@kunal.home>
2018-03-20 23:53:46 -0700

    Commit my new changes

---
+++ TestingJava/src/testingJava/TestingJava.java
@@ -0,0 +1,20 @@
+package testingJava;
+
+import java.util.Scanner;
+
+public class TestingJava {
+
+	public static void main(String[] args) {
+		Scanner kunal = new Scanner(System.in);
+		//tuna tunaObject = new tuna ();
+
+		System.out.println("Enter your name here: ");
+		String name = kunal.nextLine();
+
+		/*tunaObject.SimpleMessage(name);
+		System.out.println("Hello");*/
+
+	}
+
+
+}
